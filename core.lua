1f5bde52db86f9f34bac4b1c36c4343fd31707dc
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-02-16 19:42:04 +0100

    v1.0.9

--- core.lua
+++ core.lua
@@ -1,16 +1,21 @@
 local _, MapUtils = ...
 local dungeonMaps = {}
 dungeonMaps[246] = {"Interface\\AddOns\\MapUtils\\media\\246"} -- The Shattered Halls
-dungeonMaps[256] = {"Interface\\AddOns\\MapUtils\\media\\256-1", "Interface\\AddOns\\MapUtils\\media\\256-2"} -- Auchenaikrypta
-dungeonMaps[258] = {"Interface\\AddOns\\MapUtils\\media\\258-1", "Interface\\AddOns\\MapUtils\\media\\258-2"} -- Sethekkhallen
+dungeonMaps[256] = {"Interface\\AddOns\\MapUtils\\media\\256"} -- Auchenaikrypta
+dungeonMaps[257] = {"Interface\\AddOns\\MapUtils\\media\\257"} -- Auchenaikrypta
+dungeonMaps[258] = {"Interface\\AddOns\\MapUtils\\media\\258"} -- Sethekkhallen
+dungeonMaps[259] = {"Interface\\AddOns\\MapUtils\\media\\259"} -- Sethekkhallen
 dungeonMaps[260] = {"Interface\\AddOns\\MapUtils\\media\\260"} -- Schattenlabyrinth
 dungeonMaps[261] = {"Interface\\AddOns\\MapUtils\\media\\261"} -- Der Blutkessel
 dungeonMaps[262] = {"Interface\\AddOns\\MapUtils\\media\\262"} -- The Underbog
 dungeonMaps[263] = {"Interface\\AddOns\\MapUtils\\media\\263"} -- The Steamvault
 dungeonMaps[265] = {"Interface\\AddOns\\MapUtils\\media\\265"} -- The Slave Pens
 dungeonMaps[266] = {"Interface\\AddOns\\MapUtils\\media\\266"} -- The Botanica
-dungeonMaps[267] = {"Interface\\AddOns\\MapUtils\\media\\267-1", "Interface\\AddOns\\MapUtils\\media\\267-2"} -- The Mechanar
-dungeonMaps[269] = {"Interface\\AddOns\\MapUtils\\media\\269-1", "Interface\\AddOns\\MapUtils\\media\\269-2", "Interface\\AddOns\\MapUtils\\media\\269-3"} -- The Arcatraz
+dungeonMaps[267] = {"Interface\\AddOns\\MapUtils\\media\\267"} -- The Mechanar
+dungeonMaps[268] = {"Interface\\AddOns\\MapUtils\\media\\268"} -- The Mechanar
+dungeonMaps[269] = {"Interface\\AddOns\\MapUtils\\media\\269"} -- The Arcatraz
+dungeonMaps[270] = {"Interface\\AddOns\\MapUtils\\media\\270"} -- The Arcatraz
+dungeonMaps[271] = {"Interface\\AddOns\\MapUtils\\media\\271"} -- The Arcatraz
 dungeonMaps[272] = {"Interface\\AddOns\\MapUtils\\media\\272"} -- Managruft
 dungeonMaps[273] = {"Interface\\AddOns\\MapUtils\\media\\273"} -- The Black Morass
 dungeonMaps[274] = {"Interface\\AddOns\\MapUtils\\media\\274"} -- Old Hillsbrad Foothills
@@ -23,7 +28,7 @@ hooksecurefunc(
 		if mapID == nil then return end
 		if dungeonMaps[mapID] == nil then
 			if IsInInstance() then
-				print("Missing Dungeon Map - mapID: " .. mapID)
+				print("Missing Dungeon Map - zoneID: " .. mapID)
 			end
 
 			return
@@ -45,10 +50,10 @@ function C_Map.GetMapArtLayers(mapID)
 			tinsert(
 				result,
 				{
-					["layerWidth"] = 1024 * num / 4,
-					["layerHeight"] = 768 / 4,
-					["tileWidth"] = 1024 / 4,
-					["tileHeight"] = 768 / 4,
+					["layerWidth"] = 1008 * num / 4,
+					["layerHeight"] = 763 / 4,
+					["tileWidth"] = 1008 / 4,
+					["tileHeight"] = 763 / 4,
 					["minScale"] = 1,
 					["maxScale"] = 2.5,
 					["additionalZoomSteps"] = 6,
