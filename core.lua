168524ad7039d59cb6a84a4c82b0eaf5345a5fe4
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-02-20 01:43:09 +0100

    v1.1.0

--- core.lua
+++ core.lua
@@ -1,5 +1,10 @@
 local _, MapUtils = ...
+MapUtils:SetAddonOutput("MapUtils", 134269)
 local dungeonMaps = {}
+local raidMaps = {}
+-- Classic ERA:
+dungeonMaps[225] = {"Interface\\AddOns\\MapUtils\\media\\225"} -- The Stockade
+-- TBC:
 dungeonMaps[246] = {"Interface\\AddOns\\MapUtils\\media\\246"} -- The Shattered Halls
 dungeonMaps[256] = {"Interface\\AddOns\\MapUtils\\media\\256"} -- Auchenaikrypta
 dungeonMaps[257] = {"Interface\\AddOns\\MapUtils\\media\\257"} -- Auchenaikrypta
@@ -20,15 +25,41 @@ dungeonMaps[272] = {"Interface\\AddOns\\MapUtils\\media\\272"} -- Managruft
 dungeonMaps[273] = {"Interface\\AddOns\\MapUtils\\media\\273"} -- The Black Morass
 dungeonMaps[274] = {"Interface\\AddOns\\MapUtils\\media\\274"} -- Old Hillsbrad Foothills
 dungeonMaps[347] = {"Interface\\AddOns\\MapUtils\\media\\347"} -- Höllenfeuerbollwerk
+raidMaps[330] = {"Interface\\AddOns\\MapUtils\\media\\330"} -- Gruul's Lair
+raidMaps[331] = {"Interface\\AddOns\\MapUtils\\media\\331"} -- Magtheridon's Lair
+raidMaps[350] = {"Interface\\AddOns\\MapUtils\\media\\350"} -- Karazhan
+raidMaps[351] = {"Interface\\AddOns\\MapUtils\\media\\351"} -- Karazhan
+raidMaps[352] = {"Interface\\AddOns\\MapUtils\\media\\352"} -- Karazhan
+raidMaps[353] = {"Interface\\AddOns\\MapUtils\\media\\353"} -- Karazhan
+raidMaps[354] = {"Interface\\AddOns\\MapUtils\\media\\354"} -- Karazhan
+raidMaps[355] = {"Interface\\AddOns\\MapUtils\\media\\355"} -- Karazhan
+raidMaps[356] = {"Interface\\AddOns\\MapUtils\\media\\356"} -- Karazhan
+raidMaps[357] = {"Interface\\AddOns\\MapUtils\\media\\357"} -- Karazhan
+raidMaps[358] = {"Interface\\AddOns\\MapUtils\\media\\358"} -- Karazhan
+raidMaps[359] = {"Interface\\AddOns\\MapUtils\\media\\359"} -- Karazhan
+raidMaps[360] = {"Interface\\AddOns\\MapUtils\\media\\360"} -- Karazhan
+raidMaps[361] = {"Interface\\AddOns\\MapUtils\\media\\361"} -- Karazhan
+raidMaps[362] = {"Interface\\AddOns\\MapUtils\\media\\362"} -- Karazhan
+raidMaps[363] = {"Interface\\AddOns\\MapUtils\\media\\363"} -- Karazhan
+raidMaps[364] = {"Interface\\AddOns\\MapUtils\\media\\364"} -- Karazhan
+raidMaps[365] = {"Interface\\AddOns\\MapUtils\\media\\365"} -- Karazhan
+raidMaps[366] = {"Interface\\AddOns\\MapUtils\\media\\366"} -- Karazhan
+local missingMaps = {}
 hooksecurefunc(
 	WorldMapFrame,
 	"Show",
 	function()
 		local mapID = C_Map.GetBestMapForUnit("player")
 		if mapID == nil then return end
-		if dungeonMaps[mapID] == nil then
-			if IsInInstance() then
-				print("Missing Dungeon Map - zoneID: " .. mapID)
+		if dungeonMaps[mapID] == nil and raidMaps[mapID] == nil then
+			local inInstance, instanceType = IsInInstance()
+			if inInstance and missingMaps[mapID] == nil then
+				missingMaps[mapID] = true
+				if instanceType == "raid" and raidMaps[mapID] == nil then
+					MapUtils:INFO("Missing Raid Map - zoneID: " .. mapID .. " (Send to Developer: zoneID and RaidName)")
+				elseif instanceType == "party" and dungeonMaps[mapID] == nil then
+					MapUtils:INFO("Missing Dungeon Map - zoneID: " .. mapID .. " (Send to Developer: zoneID and DungeonName)")
+				end
 			end
 
 			return
@@ -50,10 +81,10 @@ function C_Map.GetMapArtLayers(mapID)
 			tinsert(
 				result,
 				{
-					["layerWidth"] = 1008 * num / 4,
-					["layerHeight"] = 763 / 4,
-					["tileWidth"] = 1008 / 4,
-					["tileHeight"] = 763 / 4,
+					["layerWidth"] = 1024 * num / 4,
+					["layerHeight"] = 683 / 4,
+					["tileWidth"] = 1024 / 4,
+					["tileHeight"] = 683 / 4,
 					["minScale"] = 1,
 					["maxScale"] = 2.5,
 					["additionalZoomSteps"] = 6,
